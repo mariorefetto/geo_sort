ff663a7607b9dd2bc83b95732ce8867e2c23380d
Mario Refetto <mario.refetto@skylabs.it>
2024-03-08 15:21:24 +0100

    Add documentation

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,6 +1,16 @@
 import 'package:geo_sort/src/utils/utils.dart';
 
+/// A utility class for sorting a list of items by their distance from a reference location.
 class GeoSort {
+  /// Sorts the list of items by their distance from the reference location.
+  ///
+  /// The [items] parameter is the list of items to be sorted.
+  /// The [latitude] and [longitude] parameters specify the coordinates of the reference location.
+  /// The [ascending] parameter indicates whether to sort the items in ascending order (default) or descending order.
+  /// The [maxDistance] parameter specifies the maximum distance allowed for items to be included in the sorted list.
+  /// The [maxElements] parameter specifies the maximum number of elements to return in the sorted list.
+  ///
+  /// Returns a sorted list of items.
   static List<T> sortByLatLong<T>({
     required List<T> items,
     required double latitude,
@@ -32,6 +42,12 @@ class GeoSort {
     return distanceItemList.map((e) => e.value).toList();
   }
 
+  /// Retrieves the double value of a property from an object.
+  ///
+  /// The [object] parameter is the object from which to retrieve the property value.
+  /// The [propertyName] parameter is the name of the property.
+  ///
+  /// Returns the double value of the property, or null if the property is not found or is not a double.
   static double? _getDoubleValue<T>(T object, String propertyName) {
     final value = object is Map<String, dynamic>
         ? object[propertyName]
