b7170f15db1a095fd252daf0705811b6f9684082
Mario Refetto <mario.refetto@skylabs.it>
2024-03-09 09:31:40 +0100

    Implement has location

--- example/lib/src/model/location.dart
+++ example/lib/src/model/location.dart
@@ -1,25 +1,88 @@
-class Location {
+import 'dart:convert';
+
+import 'package:geo_sort/geo_sort.dart';
+
+class Location implements HasLocation {
   final int id;
   final String name;
   final String city;
-  final double lat;
-  final double long;
+  @override
+  final double latitude;
+  @override
+  final double longitude;
 
   Location({
     required this.id,
     required this.name,
     required this.city,
-    required this.lat,
-    required this.long,
+    required this.latitude,
+    required this.longitude,
   });
 
-  Map<String, dynamic> toJson() {
+  Location copyWith({
+    int? id,
+    String? name,
+    String? city,
+    double? latitude,
+    double? longitude,
+  }) {
+    return Location(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      city: city ?? this.city,
+      latitude: latitude ?? this.latitude,
+      longitude: longitude ?? this.longitude,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
     return {
       'id': id,
       'name': name,
       'city': city,
-      'lat': lat,
-      'long': long,
+      'latitude': latitude,
+      'longitude': longitude,
     };
+  }
+
+  factory Location.fromMap(Map<String, dynamic> map) {
+    return Location(
+      id: map['id']?.toInt() ?? 0,
+      name: map['name'] ?? '',
+      city: map['city'] ?? '',
+      latitude: map['latitude']?.toDouble() ?? 0.0,
+      longitude: map['longitude']?.toDouble() ?? 0.0,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Location.fromJson(String source) =>
+      Location.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Location(id: $id, name: $name, city: $city, latitude: $latitude, longitude: $longitude)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Location &&
+        other.id == id &&
+        other.name == name &&
+        other.city == city &&
+        other.latitude == latitude &&
+        other.longitude == longitude;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        city.hashCode ^
+        latitude.hashCode ^
+        longitude.hashCode;
   }
 }
